6fec8934f2345baf64c82d5cdc813911e879a7e9
jiristrouhal <MznStrouhal@gmail.com>
2026-03-23 15:46:28 +0100

    Define test_main

--- main.py
+++ main.py
@@ -1,10 +1,36 @@
-MAX_VALS = 1000
+from __future__ import annotations
+import dataclasses
 
 
-class CellVals:
-    def __init__(self) -> None:
-        self._values = [0.0] * MAX_VALS
+@dataclasses.dataclass
+class Position:
+    x: float
+    y: float
+    z: float = 0.0
+
+
+class Cell:
+    def __init__(self, position: Position) -> None:
+        self._value = 0.0
+        self.position = position
+        self._neighbours: set[Cell] = set()
 
     @property
-    def values(self) -> list[float]:
-        return self._values
+    def value(self) -> float:
+        return self._value
+
+    @property
+    def neighbours(self) -> set[Cell]:
+        return self._neighbours
+
+    def add_neighbour(self, cell: Cell) -> None:
+        assert isinstance(cell, Cell), f"Expected {Cell.__name__}, received {type(cell).__name__}"
+        self._neighbours.add(cell)
+
+    def set_to(self, value: float) -> None:
+        assert isinstance(value, float | int), (
+            f"Expected int or float, received {type(value).__name__}."
+        )
+        if not 0 <= value <= 1:
+            raise ValueError(f"Cell value must be 0 <= and <= 1, received {value}.")
+        self._value = value
--- test_main.py
+++ test_main.py
@@ -1,15 +1,43 @@
+from math import inf, nan
 import pytest
 
-from main import CellVals
+from main import Cell, Position
 
 
 @pytest.fixture
-def cell_values() -> CellVals:
-    return CellVals()
+def position() -> Position:
+    return Position(1, 2, 3)
 
 
-def test_all_cell_values_are_initially_zero(cell_values: CellVals) -> None:
-    assert all(v == 0 for v in cell_values.values)
+@pytest.fixture
+def cell(position: Position) -> Cell:
+    return Cell(position)
+
+
+def test_cell_is_initially_with_zero_value_without_neighbours(cell: Cell) -> None:
+    assert cell.value == 0
+    assert not cell.neighbours
+
+
+def test_setting_neigbours(position: Position, cell: Cell) -> None:
+    cell.add_neighbour(Cell(position))
+    assert len(cell.neighbours) == 1
+    cell.add_neighbour(Cell(position))
+    assert len(cell.neighbours) == 2
+
+
+@pytest.mark.parametrize("new_value", [1, 0, 0.1, 0.5])
+def test_cell_value_can_be_set_to_any_probability_value(cell: Cell, new_value: float) -> None:
+    cell.set_to(new_value)
+    assert cell.value == new_value
+
+
+@pytest.mark.parametrize("new_value", [-1, -0.1, 5, nan, inf, -inf])
+def test_setting_cell_value_outside_probability_range_raises_error(
+    cell: Cell, new_value: float
+) -> None:
+    with pytest.raises(ValueError):
+        cell.set_to(new_value)
 
 
 if __name__ == "__main__":
